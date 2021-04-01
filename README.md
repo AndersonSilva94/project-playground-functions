d58ff65f2583385e368a58fd8a2bc53900966bef
Anderson Silva <andssilva.23@gmail.com>
2021-04-01 13:37:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## Projeto Playground Function :memo:
+
+### Segundo projeto do curso de Desenvolvimento Full Stack Web da Trybe.
+
+Objetivos a serem alcançados no decorrer da construção do projeto:
+
+- Exercitar o conteúdo adquirido até sobre JavaScript.
+
+Como foi construído:
+
+- Escrevendo códigos em JavaScript que usam variáveis e tipos primitivos;
+
+- Utilizando conceitos da linguagem como a tipagem dinâmica e operadores lógicos/aritméticos/de atribuição no seu código;
+
+- Criando códigos que usam estruturas condicionais, como o if/else.
+
+- Manipular arrays (listas);
+
+- Utilizando o comando for;
+
+- Quebrando grandes problemas em pequenos;
+
+- Utilizando a lógica de programação na resolução de problemas;
+
+- Manipulando objetos;
+
+- Utilizando o comando for/in;
+
+- Utilizando funções para organizar e estruturar o seu código;
---
+++ challenges2.js
@@ -0,0 +1,36 @@
+// Desafio 10
+// lógica para ordenar a lista através do sort() encontrada em https://pt.stackoverflow.com/questions/46600/como-ordenar-uma-array-de-objetos-com-array-sort
+function techList(array, name) {
+  let newArrayTech = [];
+  for (let index = 0; index < array.length; index += 1) {
+    let itemListTech = {
+      tech: array[index],
+      name,
+    };
+    newArrayTech.push(itemListTech);
+  }
+  if (array.length === 0) {
+    return 'Vazio!';
+  } return newArrayTech.sort(function (tech1, tech2) {
+    if (tech1.tech < tech2.tech) {
+      return -1;
+    } return 1;
+  });
+}
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
+console.log(techList([], 'Anderson'));
+
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
+
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
+
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
